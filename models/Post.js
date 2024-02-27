b800d51cb14148b3ef8649dc2834d176b8c6b2dc
Krystian Rusin <36743674+KrystianRusin@users.noreply.github.com>
2024-02-26 21:17:33 -0500

    progress towards uploading and storing images

--- models/Post.js
+++ models/Post.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const PostSchema = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
   comment: String,
+  image: String,
   Date: { type: Date, default: Date.now },
 });
 
--- routes/posts.js
+++ routes/posts.js
@@ -1,13 +1,18 @@
 const express = require("express");
 const Post = require("../models/Post");
 const router = express.Router();
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
 
-router.post("/create", async (req, res) => {
+router.post("/create", upload.single("image"), async (req, res) => {
+  // use multer middleware
   const { user, comment } = req.body;
+  const image = req.file.path; // get the path of the uploaded file
 
   const post = new Post({
     user,
     comment,
+    image, // store the path in the database
     Date: new Date(),
   });
   await post.save();
