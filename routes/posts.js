bf0cba7cce47154ff5b92617b3313279555ebb57
Krystian Rusin <36743674+KrystianRusin@users.noreply.github.com>
2024-02-27 22:27:44 -0500

    added aws s3 integration

--- routes/posts.js
+++ routes/posts.js
@@ -3,16 +3,35 @@ const Post = require("../models/Post");
 const router = express.Router();
 const multer = require("multer");
 const upload = multer({ dest: "uploads/" });
+const AWS = require("aws-sdk");
+
+AWS.config.update({
+  accessKeyId: process.env.AWS_ACCESS_KEY,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+});
+
+const s3 = new AWS.S3();
+
+const params = {
+  Bucket: process.env.AWS_BUCKET_NAME,
+};
 
 router.post("/create", upload.single("image"), async (req, res) => {
+  s3.listObjects(params, function (err, data) {
+    if (err) {
+      console.log("Error", err);
+    } else {
+      console.log("Success", data);
+    }
+  });
   // use multer middleware
   const { user, comment } = req.body;
-  const image = req.file.path; // get the path of the uploaded file
+  //const image = req.file.path; // get the path of the uploaded file
 
   const post = new Post({
     user,
     comment,
-    image, // store the path in the database
+    // image, // store the path in the database
     Date: new Date(),
   });
   await post.save();
