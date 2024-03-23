8ee6bea2d3d3406f02cefc78e8388d98171ff4b3
Krystian Rusin <36743674+KrystianRusin@users.noreply.github.com>
2024-03-22 20:07:26 -0400

    added route to search database for users matching search query

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
-const { ObjectId } = require("mongodb");
 
 router.get("/:id", async (req, res) => {
   const id = req.params.id;
@@ -10,6 +9,18 @@ router.get("/:id", async (req, res) => {
     res.json(user);
   } catch (error) {
     res.json({ message: "An error occurred while fetching user data" });
+  }
+});
+
+router.get("/search/:searchTerm", async (req, res) => {
+  const searchTerm = req.params.searchTerm;
+  try {
+    const users = await User.find({
+      username: { $regex: searchTerm, $options: "i" },
+    });
+    res.json(users);
+  } catch (error) {
+    res.json({ message: "An error occurred while searching for users" });
   }
 });
 
