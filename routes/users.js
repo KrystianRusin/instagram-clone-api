7fab3ae68b81eec8dc205f7a7b5d74cd4fe769a6
Krystian Rusin <36743674+KrystianRusin@users.noreply.github.com>
2024-04-01 22:57:45 -0400

    fixed bug that caused server to crash if file was not uploaded

--- routes/users.js
+++ routes/users.js
@@ -85,36 +85,39 @@ router.put("/:userId/update", upload.single("avatar"), async (req, res) => {
   const { userId } = req.params;
   const { bio } = req.body;
   const file = req.file; // this is the uploaded avatar file
-  console.log("USERID ", userId);
-  console.log(file);
-  console.log("BIO ", bio);
+
   try {
-    // Create a new blob in the bucket and upload the file data
-    const blob = bucket.file(file.originalname);
-    const blobStream = blob.createWriteStream();
+    if (file) {
+      // A file was uploaded, so upload it to Firebase
+      const blob = bucket.file(file.originalname);
+      const blobStream = blob.createWriteStream();
 
-    blobStream.on("error", (err) => {
-      console.error("Error uploading to Firebase:", err);
-      return res.status(500).json({ error: "Error uploading to Firebase" });
-    });
+      blobStream.on("error", (err) => {
+        console.error("Error uploading to Firebase:", err);
+        return res.status(500).json({ error: "Error uploading to Firebase" });
+      });
 
-    blobStream.on("finish", async () => {
-      // The public URL can be used to directly access the file via HTTP.
-      const publicUrl = `https://firebasestorage.googleapis.com/v0/b/${
-        bucket.name
-      }/o/${encodeURIComponent(blob.name)}?alt=media`;
+      blobStream.on("finish", async () => {
+        const publicUrl = `https://firebasestorage.googleapis.com/v0/b/${
+          bucket.name
+        }/o/${encodeURIComponent(blob.name)}?alt=media`;
 
-      // Update the user document with the new bio and profilePic
-      const user = await User.findByIdAndUpdate(
-        userId,
-        { bio, profilePic: publicUrl },
-        { new: true }
-      );
+        const user = await User.findByIdAndUpdate(
+          userId,
+          { bio, profilePic: publicUrl },
+          { new: true }
+        );
+
+        res.json(user);
+      });
+
+      blobStream.end(file.buffer);
+    } else {
+      // No file was uploaded, so just update the bio
+      const user = await User.findByIdAndUpdate(userId, { bio }, { new: true });
 
       res.json(user);
-    });
-
-    blobStream.end(file.buffer);
+    }
   } catch (error) {
     console.error(error);
     res.json({ message: "An error occurred while updating user data" });
